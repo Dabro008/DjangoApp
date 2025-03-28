5e37400b3be57623f6377dd342f8bad766016d3c
Dabro008 <alex008.job@gmail.com>
2025-03-28 08:24:03 +0300

    Login form and controller are ready

---
+++ users/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm
+
+from users.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+    class Mets:
+        model = User
+        fields = ["username", "password"]
